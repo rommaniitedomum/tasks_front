d900eb7f2b7996b32d26dd2ec9039b94c3a90201
rommaniitedomum <qkrwns982@gmail.com>
2024-11-18 15:44:43 +0900

    v5

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,5 +1,5 @@
 // const rootPath = "http://localhost:8000";
-const rootPath = "http://54.90.161.6";
+const rootPath = "https://back.aicc4park.co.kr";
 
 const GET_TASKS_API_URL = `${rootPath}/getTasks`;
 const DELETE_TASK_API_URL = `${rootPath}/deleteTask`;
